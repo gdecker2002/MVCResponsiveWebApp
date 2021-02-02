50f7b5af6ae1db65022b8816316d50e373eefa82
Gage <gdecker@dmacc.edu>
2021-02-01 21:25:24 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MVCResponsiveWebApp.Models;
+
+namespace Ch02FutureValueDecker.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.FV = 0;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(ResponsiveModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.FV = model.AgeThisYear();
+            }
+            else
+            {
+                ViewBag.FV = 0;
+            }
+            return View(model);
+        }
+
+    }
+}
---
+++ Models/ResponsiveModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace MVCResponsiveWebApp.Models
+{
+    public class ResponsiveModel
+    {
+        public string userName { get; set; }
+        [Required(ErrorMessage = "Please enter a 4 digit number.")]
+        [Range(1900, 2021, ErrorMessage = "Number of years must be between 1900 and 2021.")]
+        public int? yearOfBirth { get; set; }
+        public int AgeThisYear()
+        {
+            int _CURRENTYEAR = 2021;
+            int age = _CURRENTYEAR - yearOfBirth.Value;
+            return age;
+        }
+        
+
+    }
+}
